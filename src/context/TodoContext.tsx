255261433c54b8eb65d359b986483dbffa04579a
KunwarVSuryavanshi <virajkunwar98@gmail.com>
2023-06-08 23:22:29 +0530

    :tada: expand and mark todo

--- src/context/TodoContext.tsx
+++ src/context/TodoContext.tsx
@@ -4,17 +4,25 @@ export interface Todo {
   id: number;
   text: string;
   subTasks?: Todo[];
+  expanded: boolean;
+  status: boolean;
 }
 
 interface TodoContextProps {
   todos: Todo[];
-  addTodo: (text: string, parentID?: number) => void;
+  addTodo: (text: string, parentID?: number) => void; //for adding tasks
+  expandTodo: (id: number) => void; //for maintaining the expanded state of a task
+  markTodo: (id: number) => void; //for marking a task as done
 }
 
 export const TodoContext = createContext<TodoContextProps>({
   todos: [],
   // eslint-disable-next-line @typescript-eslint/no-empty-function
   addTodo: () => { },
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  expandTodo: () => { },
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  markTodo: () => { },
 });
 
 const searchTodo = (todos: Todo[], id: number): Todo | undefined => {
@@ -29,6 +37,17 @@ const searchTodo = (todos: Todo[], id: number): Todo | undefined => {
   return result;
 }
 
+const markDone = (todos: Todo): void => {
+  if (todos && todos.subTasks) {
+    todos.subTasks.forEach((subTask) => {
+      subTask.status = todos.status;
+      if (subTask.subTasks)
+        markDone(subTask);
+    });
+  }
+}
+
+
 export const TodoProvider = ({ children }: { children: ReactNode }) => {
   const [todos, setTodos] = useState<Todo[]>([]);
 
@@ -36,14 +55,18 @@ export const TodoProvider = ({ children }: { children: ReactNode }) => {
     const newTodo: Todo = {
       id: Date.now(),
       text: text,
+      expanded: true,
+      status: false,
     };
     if (parentID) {
       const parentTodo = searchTodo(todos, parentID);
       if (parentTodo) {
         if (parentTodo.subTasks) {
           parentTodo.subTasks.push(newTodo);
+          parentTodo.status = false;
         } else {
           parentTodo.subTasks = [newTodo];
+          parentTodo.status = false;
         }
       }
     } else {
@@ -51,8 +74,33 @@ export const TodoProvider = ({ children }: { children: ReactNode }) => {
     }
   };
 
+  const expandTodo = (id: number) => {
+    if (id) {
+      const todo = searchTodo(todos, id);
+      if (todo) {
+        todo.expanded = !todo.expanded;
+        setTodos([...todos]);
+      }
+    }
+  };
+
+  const markTodo = (id: number) => {
+    if (id) {
+      const todo = searchTodo(todos, id);
+      if (todo) {
+        todo.status = !todo.status;
+        if (todo.subTasks) {
+          // todo.subTasks.forEach((subTask) => {
+          //   subTask.status = todo.status;
+          // });
+          markDone(todo);
+        }
+        setTodos([...todos]);
+      }
+    }
+  };
   return (
-    <TodoContext.Provider value={{ todos, addTodo }}>
+    <TodoContext.Provider value={{ todos, addTodo, expandTodo, markTodo }}>
       {children}
     </TodoContext.Provider>
   );
